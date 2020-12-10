412dec7ebe9a6c795b00fca46a2559fc6b0ed6ac
cherrerab <54047538+cherrerab@users.noreply.github.com>
2020-12-09 23:24:10 -0300

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from utils._tools import *
---
+++ utils/_tools.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from sklearn.metrics import confusion_matrix
+
+from scipy.stats import kurtosis
+from scipy.stats import skew
+
+# ----------------------------------------------------------------------------
+def plot_confusion_matrix(Y_true, Y_pred, target_names,
+                          title='Confusion matrix',
+                          cmap=None, normalize=False,
+                          figsize=(5,5)):
+    
+    """
+    given the true (Y_true) and the predicted (Y_pred) labels,
+    makes the confusion matrix.
+    
+    :param np.array Y_true:
+        the true labels of the data. (no one hot encoding).
+    :param np.array Y_pred:
+        the predicted labels of the data by the model. (no one hot encoding).
+    :param list target_names:
+        given classification classes such as [0, 1, 2] the class names,
+        for example: ['high', 'medium', 'low'].
+    :param str title:
+        the text to display at the top of the matrix.
+    :param str cmap:
+        the gradient of the values displayed from matplotlib.pyplot.cm
+        see http://matplotlib.org/examples/color/colormaps_reference.html
+        plt.get_cmap('jet') or plt.cm.Blues.
+    :param bool normalize:
+        if False, plot the raw numbers, if True, plot the proportions.
+        
+    :reference:
+        http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+        
+    """
+    import itertools
+    
+    cm = confusion_matrix(Y_true, Y_pred)
+    
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+    
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+    
+    plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                      verticalalignment="center",
+                      horizontalalignment="center",
+                      color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                      verticalalignment="center",
+                      horizontalalignment="center",
+                      color="white" if cm[i, j] > thresh else "black")
+    
+    
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+    
+# ----------------------------------------------------------------------------
+def plot_loss_function(train_info, figsize=(5,5)):
+    """
+    -> None
+    
+    this function plots de evolution of the loss function of the model 
+    during the training epochs.
+    
+    :param train_info:
+        training history of the classification model.
+        
+    """
+    # crear figura
+    plt.figure(figsize=figsize)
+    
+    plt.plot(train_info.history['loss'])
+    plt.plot(train_info.history['val_loss'])
+    
+    # caracteristicas del plot
+    plt.title('Model loss')
+    plt.ylabel('Loss'); plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper right')
+    plt.show()
+ 
+# ----------------------------------------------------------------------------
+def plot_classification_map(model, xlim, ylim, res):
+  """
+  This function plots de classification map on the specified 2D region.
+  """
+  # initialize 2D region
+  x = np.linspace(xlim[0], xlim[1], res)
+  y = np.linspace(ylim[0], ylim[1], res)
+  
+  x, y = np.meshgrid(x, y)
+  
+  X = np.zeros( (x.size, 2) )
+  
+  X[:, 0] = np.reshape(x.flatten(), (-1, 1))
+  X[:, 1] = np.reshape(y.flatten(), (-1, 1))
+
+  # generate the classification map
+  Y = model.predict(X)
+  Y_map = np.reshape( Y[:,1], (res, res))
+
+  plt.figure()
+  plt.imshow(Y_map, cmap='bwr')
+  return
