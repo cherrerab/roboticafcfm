384797b9ea36f55dbe083fb083241a359509e6fa
cherrerab <54047538+cherrerab@users.noreply.github.com>
2021-05-25 13:15:23 -0400

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,3 +6,5 @@ from utils._tools import *
 from utils.chess import plot_lines, plot_corners
 from utils.chess import get_intersections, mask_contour
 from utils.chess import ChessBoard
+
+from utils.flappy import Bird, Pipe, renderFrame
